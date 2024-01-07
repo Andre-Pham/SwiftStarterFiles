546812350c2fadd2fdadf3f950147353f445b1f8
Andre Pham <andrepham33@gmail.com>
2024-01-08 00:37:53 +1100

    Clonable

---
+++ Clonable.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+protocol Clonable {
+
+    init(_ original: Self)
+    
+}
+extension Clonable {
+    
+    func clone() -> Self {
+        return type(of: self).init(self)
+    }
+    
+}
+extension Array where Element: Clonable {
+    
+    func clone() -> Array {
+        var clonedArray = Array<Element>()
+        for element in self {
+            clonedArray.append(element.clone())
+        }
+        return clonedArray
+    }
+    
+}
+extension Dictionary where Value: Clonable {
+    
+    func clone() -> Dictionary {
+        var clonedDictionary = Dictionary<Key, Value>()
+        for pair in self {
+            clonedDictionary[pair.key] = pair.value.clone()
+        }
+        return clonedDictionary
+    }
+    
+}
